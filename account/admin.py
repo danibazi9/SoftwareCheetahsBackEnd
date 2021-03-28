13a7311ed8da942c0890ff86987b8ebee3c06268
Danial <danibazi9@gmail.com>
2021-03-28 13:52:23 +0430

    Cusomize django administration for account app

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
+import datetime
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from account.models import Account
+from persiantools.jdatetime import JalaliDateTime
+
+
+class AccountAdmin(UserAdmin):
+	list_display = ('user_id', 'first_name', 'last_name', 'email', 'get_date_joined', 'role', 'is_admin')
+	search_fields = ('first_name', 'last_name', 'email')
+	list_filter = ('role',)
+	exclude = ('password',)
+	readonly_fields = ('date_joined', 'last_login')
+
+	filter_horizontal = ()
+	fieldsets = ()
+
+	def get_date_joined(self, obj):
+		timestamp = datetime.datetime.timestamp(obj.date_joined)
+		jalali_datetime = JalaliDateTime.fromtimestamp(timestamp)
+		return jalali_datetime.strftime("%Y/%m/%d - %H:%M")
+
+
+admin.site.register(Account, AccountAdmin)
