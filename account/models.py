84165b042162509c94ab1071648d891604eab513
Danial <danibazi9@gmail.com>
2021-03-28 13:47:42 +0430

    Create model account for user info

--- account/models.py
+++ account/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have a username')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            username=username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.first_name = 'admin'
+        user.last_name = 'admin'
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    # required fields
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    password = models.CharField(max_length=20, blank=True)
+
+    # optional fields
+    national_code = models.IntegerField(unique=True, blank=True)
+
+    GENDER_CHOICES = ('Male', 'Female')
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    birthday = models.DateField(blank=True)
+    image = models.ImageField(upload_to='users/images/', blank=True)
+    bio = models.TextField(blank=True)
+
+    # auto-generate fields
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    # For checking permissions. to keep it simple all admin have ALL permissions
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    # Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
+    def has_module_perms(self, app_label):
+        return True
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
