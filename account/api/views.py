34d86218456eb6d974c18ca8562c447526a7aea4
Danial Bazmandeh <danibazi9@gmail.com>
2021-03-31 00:06:29 +0430

    Remove function CheckUniqueness

--- account/api/views.py
+++ account/api/views.py
@@ -186,27 +186,3 @@ def send_email(request):
         return Response({"vc_code": random_code_generated}, status=status.HTTP_200_OK)
     else:
         return Response(f"The email '{email}' doesn't exist", status=status.HTTP_406_NOT_ACCEPTABLE)
-                            
-
-@api_view(['POST' , ])
-def checkUniqueness(request):
-    if request.method == 'POST':
-        data = dict(request.POST)
-        errors = {}
-        errors_count = 0
-        if 'username' in data.keys():
-            if list(Account.objects.filter(username=data['username'][0])) != []:
-                errors['username'] = 'This username already exist'
-                errors_count += 1
-        if 'email' in data.keys():
-            if list(Account.objects.filter(email=data['email'][0])) != []:
-                errors['email'] = 'This email already exist'
-                errors_count += 1
-        if 'phone' in data.keys():
-            if list(Account.objects.filter(phone=data['phone'][0])) != []:
-                errors['phone'] = 'This phone already exist'
-                errors_count += 1
-        if errors_count > 0:
-            return JsonResponse({'errors' : errors}, status=status.HTTP_404_NOT_FOUND)    
-        else :
-            return JsonResponse({},status=status.HTTP_200_OK) 
