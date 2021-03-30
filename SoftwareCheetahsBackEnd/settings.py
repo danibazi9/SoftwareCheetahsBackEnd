def9f21406eaedd4f8fb6afdaff6f4e7a8cd3fff
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2021-03-31 00:39:37 +0430

    edit setting

--- SoftwareCheetahsBackEnd/settings.py
+++ SoftwareCheetahsBackEnd/settings.py
@@ -139,8 +139,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticsfile')
 
-MEDIA_URL = '/media/'
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = '/static/'
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
