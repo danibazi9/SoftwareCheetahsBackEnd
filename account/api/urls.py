e772b36f18421d95bfbb08d31392f0cbed0d3620
Danial Bazmandeh <danibazi9@gmail.com>
2021-03-31 00:07:05 +0430

    Remove url for check Uniqueness

--- account/api/urls.py
+++ account/api/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('properties', account_properties_view, name="properties"),
     path('send-email', send_email, name='send_email'),
     path('properties/all', all_accounts_view, name="properties_all"),
-    path('checkUniqueness/', checkUniqueness, name="checkUniqueness"),
 ]
