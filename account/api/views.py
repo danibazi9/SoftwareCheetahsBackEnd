a0aa9459d2cb84faacf039d76d356934e6e07948
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2021-04-03 10:59:25 +0430

    update database

--- account/api/views.py
+++ account/api/views.py
@@ -61,6 +61,7 @@ def registration_view(request):
 
 @api_view(['GET', ])
 def account_properties_view(request):
+    account = request.user
     try:
         account = request.user
     except Account.DoesNotExist:
