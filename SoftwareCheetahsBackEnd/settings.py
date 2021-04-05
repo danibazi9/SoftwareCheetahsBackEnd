7dbd1a6e0790931e191805b95a0cf4fe8d9cc98b
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2021-04-05 17:04:40 +0430

    add static file

--- SoftwareCheetahsBackEnd/settings.py
+++ SoftwareCheetahsBackEnd/settings.py
@@ -54,7 +54,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
