6d1c183531d0f109f823a232bc0ec8bde168e939
Danial <danibazi9@gmail.com>
2021-04-26 14:02:02 +0430

    Write unit tests for upload image for villa API

--- villa/tests/test_views.py
+++ villa/tests/test_views.py
@@ -67,3 +67,77 @@ class CheckGetAllVillas(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
+class UploadImageTest(TestCase):
+    """ Test module for uploading a new image in database """
+
+    def setUp(self):
+        new_user = Account.objects.create(
+            first_name='Danial',
+            last_name='Bazmandeh',
+            email='danibazi9@gmail.com',
+            phone_number='+989152147655',
+            gender='Male',
+            password='123456'
+        )
+
+        self.valid_token, self.created = Token.objects.get_or_create(user=new_user)
+        self.invalid_token = 'fasdfs45dsfasd1fsfasdf4dfassf13'
+
+        valid_image = open(os.path.join(BASE_DIR, 'danial.jpg'), 'rb')
+        invalid_image = open(os.path.join(BASE_DIR, 'file.pdf'), 'rb')
+
+        self.valid_upload = {
+            'file': valid_image
+        }
+
+        self.invalid_upload = {
+        }
+
+        self.invalid_upload2 = {
+            'imageddf32': valid_image
+        }
+
+        self.invalid_upload3 = {
+            'file': invalid_image
+        }
+
+    def test_upload_valid_image(self):
+        response = client.post(
+            reverse('villa:upload_image'),
+            data=self.valid_upload,
+            HTTP_AUTHORIZATION='Token {}'.format(self.valid_token),
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_upload_invalid_image(self):
+        response = client.post(
+            reverse('villa:upload_image'),
+            data=self.invalid_upload,
+            HTTP_AUTHORIZATION='Token {}'.format(self.valid_token),
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = client.post(
+            reverse('villa:upload_image'),
+            data=self.invalid_upload2,
+            HTTP_AUTHORIZATION='Token {}'.format(self.valid_token),
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = client.post(
+            reverse('villa:upload_image'),
+            data=self.invalid_upload3,
+            HTTP_AUTHORIZATION='Token {}'.format(self.valid_token),
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_upload__image_unauthorized(self):
+        response = client.post(
+            reverse('villa:upload_image'),
+            data=self.valid_upload,
+            HTTP_AUTHORIZATION='Token {}'.format(self.invalid_token),
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
