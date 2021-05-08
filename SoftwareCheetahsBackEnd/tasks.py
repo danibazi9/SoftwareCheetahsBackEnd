6651fc01df1bdc0a92d797ffbbd3a0f1dab1f766
Danial Bazmandeh <danibazi9@gmail.com>
2021-05-08 17:56:38 +0430

    Comment tasks.py

--- SoftwareCheetahsBackEnd/tasks.py
+++ SoftwareCheetahsBackEnd/tasks.py
@@ -1,6 +1,6 @@
-from villa.models import Image
+# from villa.models import Image
 
 
-def remove_waste_images():
-    images_to_remove = Image.objects.filter(villa=None)
-    images_to_remove.delete()
+# def remove_waste_images():
+#     images_to_remove = Image.objects.filter(villa=None)
+#     images_to_remove.delete()
