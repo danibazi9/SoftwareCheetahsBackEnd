55b7befddb04ce25234ecfafd57260136804cac2
Danial <danibazi9@gmail.com>
2021-07-11 09:37:54 +0430

    Setup Cronjob for scheduling push notification for cancel reservation

--- SoftwareCheetahsBackEnd/cron.py
+++ SoftwareCheetahsBackEnd/cron.py
@@ -26,3 +26,23 @@ def reminder_register():
                                          )
             except Exception as e:
                 print(f"Error: {e}")
+
+
+def reminder_cancel_reservation():
+    reserved_rows = Calendar.objects.filter(start_date__gte=datetime.datetime.now().date())
+    for reserved_row in reserved_rows:
+        if reserved_row.start_date - datetime.datetime.now().date() >= 10:
+            try:
+                customer_device = GCMDevice.objects.get(user=reserved_row.customer)
+                host_device = GCMDevice.objects.get(user=reserved_row.villa.owner)
+
+                customer_device.send_message(title=f"You can cancel your reservation "
+                                                   f"without loosing money until tonight!",
+                                             message=f"You've reserved '{reserved_row.villa.name}' in "
+                                                     f"{reserved_row.villa.country}, {reserved_row.villa.state}, "
+                                                     f"{reserved_row.villa.city} "
+                                                     f"for {reserved_row.start_date}, {reserved_row.end_date}"
+                                             )
+
+            except Exception as e:
+                print(f"Error: {e}")
